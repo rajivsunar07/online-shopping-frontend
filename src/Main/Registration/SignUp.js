3ccc2117dfc214211ac89509fce0a1ebf0870507
rajivsunar <rajivsunar07@gmail.com>
2021-08-25 15:21:09 +0545

    Signup component added

---
+++ src/Main/Registration/SignUp.js
@@ -0,0 +1,51 @@
+import { React, Component } from "react";
+import axios from 'axios';
+
+class SignUp extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            email: null,
+            password: null,
+            name: null
+        }
+    }
+
+    handleSubmit = (e)=> {
+        e.preventDefault()
+
+        axios.post('http://localhost:5000/user/register', this.state)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    handleChange = (e)=>{
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render(){
+        const {email, password} = this.state
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
+                    <input type="text" placeholder="Email Address" name="email" onChange={this.handleChange} />
+                    <input type="password" placeholder="Password" name="password" onChange={this.handleChange} />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+
+    
+    
+}
+
+export default SignUp
