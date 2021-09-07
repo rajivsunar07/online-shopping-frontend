ae0940573176dc3f5a6e94c363d77f50792d61b3
rajivsunar <rajivsunar07@gmail.com>
2021-09-07 10:51:57 +0545

    Navbar logged in user name display function

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,8 +1,57 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, NavLink, Link } from 'react-router-dom'
+import axios from 'axios'
+import { React, Component } from 'react'
+import { BrowserRouter as Router, NavLink, withRouter} from 'react-router-dom'
 
 export class Header extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            userName: null
+        }
+
+    }
+
+    componentDidMount(){
+        if(localStorage.getItem('token')){
+            axios.get('http://localhost:5000/user/', { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') }})
+            .then(res => {
+                this.setState({
+                    userName: res.data.user.name
+                })
+            })
+
+        }
+    }
+
+    logout = (e) => {
+        e.preventDefault()
+        localStorage.removeItem('token')
+        this.props.history.push('/')
+    }
+
     render() {
+        if(localStorage.getItem('token')){
+
+
+            var links = (<>
+                            <li className="nav-item">
+                                <NavLink to="/product/create" className="nav-link" >Create product</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="#" className="nav-link" >{this.state.userName}</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <a href="" className="nav-link" onClick={this.logout}>Logout</a>
+                            </li>
+                          </>)
+            
+        }else{
+            var links = <li className="nav-item">
+                            <NavLink exact className="nav-link" to="/login" >Login</NavLink>
+                        </li>
+        }
+
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -12,13 +61,7 @@ export class Header extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/product/create">Create Product</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/login" >Login</NavLink>
-                            </li>
-                    
+                            {links}
                         </ul>
                     </div>
                 </nav>
@@ -28,4 +71,4 @@ export class Header extends Component {
     }
 }
 
-export default Header
+export default withRouter(Header)
