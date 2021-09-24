b6f9c0cc2f334cad9e7d1dde1d1b1c201e4ab7e1
rajivsunar <rajivsunar07@gmail.com>
2021-09-24 14:33:00 +0545

    Accept request function added

--- src/Component/Main/Order/ExchangeRequests.js
+++ src/Component/Main/Order/ExchangeRequests.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { BrowserRouter as Router, withRouter } from 'react-router'
 
 export class ExchangeRequests extends Component {
 
@@ -13,16 +14,34 @@ export class ExchangeRequests extends Component {
     }
 
     componentDidMount() {
-        axios.get(`/exchangeProduct/`, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
+
+        axios.get(`/exchangeProduct/${this.props.match.params.for}`, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
             .then(res => {
                 this.setState({
                     requests: res.data.result
                 })
-                console.log(this.state)
+                
             })
             .catch(err => {
                 console.log('Error in getting exchange requests')
             })
+    }
+ 
+    
+
+
+    acceptRequest(id) {
+        let data = {
+            accepted: true
+        }
+        axios.patch(`/exchangeProduct/${id}`, data, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
+        .then(res => {
+            console.log(res.data.success)
+            this.componentDidMount()
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     render() {
@@ -56,7 +75,7 @@ export class ExchangeRequests extends Component {
                                                         <td>{request.description}</td>
                                                         <td>{request.exchangeFor.name}</td>
                                                         <td><img src={`http://localhost:5000/`+ request.exchangeFor.image[0]} alt="" /></td>
-                                                        <td>{String(request.accepted)}</td>
+                                                        <td>{request.accepted ? <>Accepted</> : <button className="btn btn-success" onClick={()=> this.acceptRequest(request._id)}>Accept</button>}</td>                                     
                                                     </tr>
                                                 </>
                                                 )
@@ -77,4 +96,4 @@ export class ExchangeRequests extends Component {
     }
 }
 
-export default ExchangeRequests
+export default withRouter(ExchangeRequests) 
