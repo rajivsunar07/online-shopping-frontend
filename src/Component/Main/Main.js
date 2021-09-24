25666810504bc26c91cf74703a5c06c6cfd75289
rajivsunar <rajivsunar07@gmail.com>
2021-09-24 14:32:29 +0545

    Order and product udpate components added

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,14 +1,21 @@
 import { React, Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 import SignUp from './User/SignUp';
 import Login from './User/Login';
 import Product from './Product/Product';
 import ProductCreate from './Product/ProductCreate';
 import ProductDetail from './Product/ProductDetail';
+import ProductUpdate from './Product/ProductUpdate';
 import Cart from './Order/Cart';
 import MyOrders from './Order/MyOrders';
-import ExchangeRequest from './Order/ExchangeRequest';
+import ExchangeRequestForm from './Order/ExchangeRequestForm';
+import ExchangeRequests from './Order/ExchangeRequests';
+
+import MyProducts from './Product/MyProducts';
+import Orders from './Order/Orders';
+
+
 
 
 
@@ -16,16 +23,26 @@ export class Main extends Component {
     render() {
         return (
             <div>
-                <Switch>
-                    <Route exact path="/signup" component={SignUp} />
-                    <Route exact path="/login" component={Login}/>
-                    <Route exact path="/" component={Product} />
-                    <Route exact path="/product/create" component={ProductCreate} />
-                    <Route exact path="/product/:id" component={ProductDetail}/>
-                    <Route exact path="/cart" component={Cart}/>
-                    <Route exact path="/myorders" component={MyOrders}/>
-                    <Route exact path="/exchange/request/:id" component={ExchangeRequest}/>
-                </Switch>
+                    <Switch>
+                        <Route exact path="/signup" component={SignUp} />
+                        <Route exact path="/login" component={Login}/>
+
+                        <Route exact path="/" component={Product} />
+                        <Route exact path="/product/create" component={ProductCreate} />
+                        <Route exact path="/product/:id" component={ProductDetail}/>
+                        <Route exact path="/product/update/:id" component={ProductUpdate}/>
+                        <Route exact path="/myproducts" component={MyProducts}/>
+                            
+                        <Route exact path="/cart" component={Cart}/>
+                        <Route exact path="/myorders" component={MyOrders}/>
+                        <Route exact path="/orders" component={Orders}/>
+
+                        <Route exact path="/exchange/request/:id/:sellerId" component={ExchangeRequestForm}/>
+                        <Route exact path="/exchange/request/:for" render={(props) => (<ExchangeRequests key={props.match.params.for}></ExchangeRequests>)} />
+
+
+
+                    </Switch>
             </div>
         )
     }
