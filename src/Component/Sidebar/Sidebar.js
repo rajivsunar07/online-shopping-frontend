43f33c0e3747e2f751795f64f3f54d5d0aa063c4
rajivsunar <rajivsunar07@gmail.com>
2021-09-25 10:22:07 +0545

    Sidebar component added

---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                This is Sidebar
+            </div>
+        )
+    }
+}
+
+export default Sidebar
