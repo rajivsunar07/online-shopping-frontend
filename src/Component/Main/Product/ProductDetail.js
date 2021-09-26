2d1d43ce3d04e0b2230eb9be0c4d1aa87d0eb405
rajivsunar <rajivsunar07@gmail.com>
2021-09-26 22:55:25 +0545

    notification added

--- src/Component/Main/Product/ProductDetail.js
+++ src/Component/Main/Product/ProductDetail.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { BrowserRouter as Router, Switch, Link } from 'react-router-dom'
+import Comment from './Comment'
+
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import 'animate.css';
 
 export class ProductDetail extends Component {
 
@@ -14,7 +19,8 @@ export class ProductDetail extends Component {
             user: null,
             price: null,
             description: null,
-            image: []
+            image: [],
+            for: []
         }
     }
 
@@ -23,7 +29,7 @@ export class ProductDetail extends Component {
         axios.get(`http://localhost:5000/product/${this.props.match.params.id}`)
             .then(result => {
                 const product = result.data.result[0]
-                
+
                 axios.get(`http://localhost:5000/user/${product.user}`)
                     .then(result => {
                         var user = result.data.result
@@ -34,7 +40,8 @@ export class ProductDetail extends Component {
                             user: user.name,
                             price: product.price,
                             description: product.description,
-                            image: product.image
+                            image: product.image,
+                            for: product.for
                         })
                     })
                     .catch(err => {
@@ -54,16 +61,25 @@ export class ProductDetail extends Component {
             product: this.state._id,
             quantity: '1',
             price: this.state.price,
-            seller: this.state.sellerId
+            seller: this.state.sellerId,
+            for: 'sell'
         }
 
-        axios.post('/order/', formdata, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token')}})
-        .then(result => {
-            console.log(result.data.message);
-            console.log('Order created and item added sucessfully')
-        })
-        .catch()
-    
+        axios.post('/order/', formdata, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
+            .then(result => {
+                store.addNotification({
+                    message: 'Added to cart',
+                    type: 'success',                      
+                    container: 'bottom-left',
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 3000
+                    }
+                })
+            })
+            .catch()
+
     }
 
     rent = () => {
@@ -75,34 +91,76 @@ export class ProductDetail extends Component {
             for: 'rent'
         }
 
-        axios.post('/order/', formdata, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token')}})
-        .then(result => {
-            console.log(result.data.message);
-            console.log('Order created and item added sucessfully')
-        })
-        .catch()
+        axios.post('/order/', formdata, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
+            .then(result => {
+                store.addNotification({
+                    message: 'Added to cart',
+                    type: 'success',                      
+                    container: 'bottom-left',
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 3000
+                    }
+                })
+            })
+            .catch(err => {
+                store.addNotification({
+                    message: 'Error in adding to cart',
+                    type: 'warning',                      
+                    container: 'bottom-left',
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 3000
+                    }
+                })
+            })
     }
 
 
     render() {
+
+
+
         return (
-            <div>
-                <div className="border p-4 m-4 col-3">
-                    {this.state.image.map(img => {
-                        return <img className="img img-fluid" src={'http://localhost:5000/' + img} alt="" width="800" />
-                    })}
-                    <h1>{this.state.name}</h1>
-                    <h4 className="text-success">RS. {this.state.price}</h4>
-                    <h4 className="">Seller: {this.state.user}</h4>
-                    <h4 className="">Description: {this.state.description}</h4>
-                    <Link to={`/exchange/request/${this.state._id}/${this.state.sellerId}`}>
-                       <button className="btn btn-primary">Request for exchange</button> 
-                    </Link>
-                    <button className="btn btn-success" onClick={this.addToCart}>Add to cart</button>
-                    <button className="btn btn-warning" onClick={this.rent}>Rent</button>
-                    
+            <div className="p-4 m-4">
+                <div className="row justify-content-center">
+                    <div className="col-4 border p-4">
+                        {this.state.image.map(img => {
+                            return <img key={img} className="productCardImage" src={'http://localhost:5000/' + img} alt="" />
+                        })}
+                    </div>
+                    <div className="col-6 border p-4">
+
+                        <h1>{this.state.name}</h1>
+                        <h4 className="text-success">RS. {this.state.price}</h4>
+                        <h4 className="">Seller: {this.state.user}</h4>
+                        <h4 className="">Description: {this.state.description}</h4>
+                        {this.state.for.includes('sell') ? <button className="btn btn-success m-1" onClick={this.addToCart}>Add to cart</button> : <></>}
+                        {this.state.for.includes('rent') ? <button className="btn btn-warning m-1" onClick={this.rent}>Rent</button> : <></>}
+                        {this.state.for.includes('rent') ? (
+                        <Link to={`/exchange/request/${this.state._id}/${this.state.sellerId}`}>
+                            <button className="btn btn-primary m-1">Request for exchange</button>
+                        </Link>
+                        ) : <></>}
+
+
+                    </div>
+
                 </div>
-              
+                <div className="row justify-content-center">
+                    <div className="col-11 p-4">
+                        <div className="row">
+                            <Comment product={this.props.match.params.id}></Comment>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
             </div>
         )
     }
