424e0c799ae6573fe8e79ab7e2b102e4cb2fefb5
rajivsunar <rajivsunar07@gmail.com>
2021-09-26 22:55:12 +0545

    notification added

--- src/Component/Main/Product/ProductCard.js
+++ src/Component/Main/Product/ProductCard.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import {Router, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
+
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import 'animate.css';
 
 export class ProductCard extends Component {
     constructor(props){
         super(props)
-
 
         this.link = "/product/" + this.props.product._id
     }
@@ -17,29 +20,66 @@ export class ProductCard extends Component {
             product: this.props.product._id,
             quantity: '1',
             price: this.props.product.price,
-            seller: this.props.product.user
+            seller: this.props.product.user,
+            for: 'sell'
         }
 
         axios.post('/order/', formdata, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token')}})
-        .then(result => {
-            console.log(result.data.message);
-            console.log('Order created and item added sucessfully')
+        .then(res => {
+            if(res.data.success){
+
+                store.addNotification({
+                    message: 'Added to cart',
+                    type: 'success',                      
+                    container: 'bottom-left',
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 3000
+                    }
+                })
+            }
         })
-        .catch()
+        .catch(err => {
+            store.addNotification({
+                message: 'Failed to add to cart',
+                type: 'warning',                         // 'default', 'success', 'info', 'warning'
+                container: 'bottom-left',
+                animationIn: ["animated", "fadeIn"],
+                animationOut: ["animated", "fadeOut"],
+                dismiss: {
+                    duration: 3000
+                }
+            })
+        })
 
     }
 
     render() {
 
         return (
-            <div className="border p-4 m-4 col-3">
-                <Link to={this.link}>
-                    <img className="img-thumbnail" src={'http://localhost:5000/' + this.props.product.image[0]} alt="" width="500dp"/>
-                    <h1>{this.props.product.name}</h1> 
-                    <h4 className="text-success">RS. {this.props.product.price}</h4> 
+            <div className="border m-4 col-7 col-sm-4 col-md-3  col-lg-2 shadow p-4">
+                <div>
+                <Link to={this.link} className="productLink">
+                    <div className=" row d-flex justify-content-center border p-3">
+                        <div className="col-10  d-flex justify-content-center">
+                            <img className="align-self-center productCardImage" src={'http://localhost:5000/' + this.props.product.image[0]} alt="" />
+
+                        </div>
+
+                   
+                    </div>
+
+
+                    <p className="text-dark mt-2">{this.props.product.name}</p> 
+                    <h6 className="text-success">RS. {this.props.product.price}</h6> 
+                    
                 </Link>
-                <button className="btn btn-success" onClick={this.addToCart}>Add to cart</button>
+                <button className="btn btn-warning" onClick={this.addToCart}><i class="fas fa-cart-plus"></i>Add to cart</button>
+            
+                </div>
             </div>
+           
             
         
         )
